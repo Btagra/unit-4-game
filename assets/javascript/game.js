13d9e204d73796b089dd8de5a147e2ad5612b953
Bharti <tagra.bharti@gmail.com>
2018-11-12 22:07:00 -0600

    added new html file

---
+++ assets/javascript/game.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+    var computerChoice = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    console.log(computerChoice);
+    $("#random").html("Random Number:" + computerChoice);
+
+    var wins = 0;
+    var losses = 0;
+    var score = 0;
+    $("#wins").html("Wins: " + wins);
+    $("#losses").html("Losses: " + losses);
+    $(".scoreDisplay").html(score);
+
+    var redCrystal = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    console.log("Red Crystal: " + redCrystal);
+    var imageCrystal = $("<img>");
+    imageCrystal.addClass("crystal-image");
+    imageCrystal.attr("src", "./assets/images/red.png");
+    imageCrystal.attr("data-crystalvalue", redCrystal);
+    $("#image-crystal").append(imageCrystal);
+
+    var blueCrystal = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    console.log("Blue Crystal: " + blueCrystal);
+    var imageCrystal = $("<img>");
+    imageCrystal.addClass("crystal-image");
+    imageCrystal.attr("src", "./assets/images/blue.png");
+    imageCrystal.attr("data-crystalvalue", blueCrystal);
+    $("#image-crystal").append(imageCrystal);
+
+    var yellowCrystal = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    console.log("Yellow Crystal: " + yellowCrystal);
+    var imageCrystal = $("<img>");
+    imageCrystal.addClass("crystal-image");
+    imageCrystal.attr("src", "./assets/images/yellow.png");
+    imageCrystal.attr("data-crystalvalue", yellowCrystal);
+    $("#image-crystal").append(imageCrystal);
+
+    var greenCrystal = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    console.log("Green Crystal: " + greenCrystal);
+    var imageCrystal = $("<img>");
+    imageCrystal.addClass("crystal-image");
+    imageCrystal.attr("src", "./assets/images/green.png");
+    imageCrystal.attr("data-crystalvalue", greenCrystal);
+    $("#image-crystal").append(imageCrystal);
+
+    $(".crystal-image").on("click", function () {
+        var crystalValue = parseInt($(this).attr("data-crystalvalue"));
+        score += crystalValue;
+        console.log("New Score: " + score);
+        $(".scoreDisplay").html(score);
+        if (score === computerChoice) {
+            wins++;
+            $("#wins").html("Wins: " + wins);
+            computerChoice = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+            $("#random").html("Random Number:" + computerChoice);
+            score = 0;
+            $(".scoreDisplay").html(score);
+        }
+
+        else if (score > computerChoice) {
+            losses++;
+            $("#losses").html("Losses: " + losses);
+            computerChoice = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+            $("#random").html("Random Number:" + computerChoice);
+            score = 0;
+            $(".scoreDisplay").html(score);
+        }
+
+    });
+
+
+});
